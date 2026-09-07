301983cd37d41f002726f0cc8f2fcf15c07ca60f
Rayhan Kabir <rayhankabir@Muhammads-Mac-mini.local>
2026-09-07 14:31:07 +0600

    user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PostModule } from './post/post.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_DB_URL!),
-    PostModule
+    PostModule,
+    UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
