6b64c433c2cc92b2a6f4d407d4268646a274c7af
Rayhan Kabir <rayhankabir@Muhammads-Mac-mini.local>
2026-09-07 14:35:04 +0600

    creaqte all module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,18 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PostModule } from './post/post.module';
 import { UserModule } from './user/user.module';
+import { CategoryModule } from './category/category.module';
+import { TagModule } from './tag/tag.module';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
   imports: [ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_DB_URL!),
     PostModule,
-    UserModule
+    UserModule,
+    CategoryModule,
+    TagModule,
+    CommentModule
   ],
   controllers: [AppController],
   providers: [AppService],
