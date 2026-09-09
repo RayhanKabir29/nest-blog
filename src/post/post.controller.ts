c19af5db03c67374ab457c1d763645ea7d407b64
Rayhan Kabir <rayhankabir@Muhammads-Mac-mini.local>
2026-09-09 14:06:46 +0600

    post post

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get,Post } from '@nestjs/common';
 import { PostService } from './post.service';
+import { CreatePostDto } from './dto/create-post.dto';
 
 @Controller('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
+
+@Post()
+  create(@Body() createPostDto: CreatePostDto) {
+    return this.postService.create(createPostDto);
+  }
 
   @Get()
   findAll() {
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -2,13 +2,20 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Post, PostDocument } from './post.schema';
 import { Model } from 'mongoose';
+import { CreatePostDto } from './dto/create-post.dto';
 
 @Injectable()
 export class PostService {
-    constructor(@InjectModel(Post.name)
-    private readonly postModel: Model<PostDocument>) {}
+  constructor(
+    @InjectModel(Post.name)
+    private readonly postModel: Model<PostDocument>,
+  ) {}
+  async create(createPostDto: CreatePostDto) {
+    const post = new this.postModel(createPostDto);
+    return post.save();
+  }
 
-    async findAll(){
-        return this.postModel.find().exec();
-    }
+  async findAll() {
+    return this.postModel.find().exec();
+  }
 }
