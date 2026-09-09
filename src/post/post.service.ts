7c1d3883628c1f7be5a4d59510adce1eed7c4311
Rayhan Kabir <rayhankabir@Muhammads-Mac-mini.local>
2026-09-09 11:40:20 +0600

    get post from mongodb using mongoose

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Post, PostDocument } from './post.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+    constructor(@InjectModel(Post.name)
+    private readonly postModel: Model<PostDocument>) {}
+
+    async findAll(){
+        return this.postModel.find().exec();
+    }
+}
